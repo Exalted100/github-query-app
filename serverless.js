2abccd77c9c509496d7c8afa79114f9734a66f37
Fawaz Haroun <harounayomikun@gmail.com>
2021-05-30 13:45:34 +0100

    debugging

--- serverless.js
+++ serverless.js
@@ -1,3 +1,6 @@
-exports.handler = async (event, context) => {
-    return process.env.token
+  exports.handler = async (event, context) => {
+    return {
+      statusCode: 200,
+      body: process.env.token
+    };
   };
