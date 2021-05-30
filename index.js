2bbf7e70aceaa194bf5571e57069cda29c0f58f6
Fawaz Haroun <harounayomikun@gmail.com>
2021-05-30 20:46:05 +0100

    debugging

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const usernameInputField = document.querySelector("#username")
+/*const usernameInputField = document.querySelector("#username")
 const submitButton = document.querySelector("#submit-button")
 const form = document.querySelector("form")
 
@@ -46,7 +46,7 @@ const getUserData = async () => {
           const result = await res.json()
           window.localStorage.setItem('userData', JSON.stringify(result))
           console.log(JSON.parse(window.localStorage.getItem("userData")).errors)
-          JSON.parse(window.localStorage.getItem("userData")).errors ? document.querySelector(".error-message-container").style.display = "flex" : location.href = "/profilePage.html"
+          JSON.parse(window.localStorage.getItem("userData")).errors ? document.querySelector(".error-message-container").style.display = "flex" : console.log("")
     } catch(err) {
         console.log(err)
     }
@@ -64,4 +64,14 @@ const removeErrorMessage = () => {
 //Form Submission Handlers
 form.addEventListener("submit", formSubmission)
 submitButton.addEventListener("click", getUserData)
-document.querySelector(".close-message").addEventListener("click", removeErrorMessage)
+document.querySelector(".close-message").addEventListener("click", removeErrorMessage)*/
+const token = "/.netlify/functions/serverless"
+
+const getToken = async () => {
+    const res = await fetch(token)
+    const result = res.json()
+    console.log(result)
+}
+
+const submitButton = document.querySelector("#submit-button")
+submitButton.addEventListener("click", getToken)
