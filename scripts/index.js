fa672c93ccef324a5097d660fa9cb72aef66838e
Fawaz Haroun <harounayomikun@gmail.com>
2021-07-18 21:36:17 +0100

    more secure app

--- scripts/index.js
+++ scripts/index.js
@@ -5,51 +5,11 @@ const closeMessage = document.querySelector(".close-message")
 
 let user;
 
-const tokenUrl = "/.netlify/functions/findToken"
-
 const getUserData = async () => {
-    try {
-        const token = await fetch(tokenUrl)
-        const parsedToken = await token.text()
-        const res = await fetch("https://api.github.com/graphql", {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-              "Authorization": `bearer ${parsedToken}`
-            },
-            body: JSON.stringify({
-              query: `
-              query {
-                  user (login: "${usernameInputField.value}") {
-                      name
-                      url
-                      avatarUrl
-                      bio
-                      login
-                          repositories (last: 20) {
-                                  nodes {
-                                      name
-                                      updatedAt
-                                      description
-                                      forkCount
-                                      url
-                                      stargazerCount
-                                      primaryLanguage {
-                                          name
-                                          color
-                                      }
-                                  }
-                          }
-                  }
-                }`
-            })
-          })
-          const result = await res.json()
-          window.localStorage.setItem("userData", JSON.stringify(result))
-          JSON.parse(window.localStorage.getItem("userData")).errors ? document.querySelector(".error-message-container").style.display = "flex" : location.href = "/profilePage.html"
-    } catch(err) {
-        console.log(err)
-    }
+    const res = await fetch("/.netlify/functions/findToken")
+    const result = await res.json()
+    window.localStorage.setItem("userData", JSON.stringify(result))
+    JSON.parse(window.localStorage.getItem("userData")).errors ? document.querySelector(".error-message-container").style.display = "flex" : location.href = "/profilePage.html"
 }
 
 //Form Submission Handlers
--- scripts/serverless/findToken.js
+++ scripts/serverless/findToken.js
@@ -1,8 +1,43 @@
+const fetch = require("node-fetch")
 const { token } = process.env
 
 exports.handler = async (event, context) => {
-  return {
-    statusCode: 200,
-    body: token,
-  }
+      const res = await fetch("https://api.github.com/graphql", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": `bearer ${token}`
+      },
+      body: JSON.stringify({
+        query: `
+        query {
+            user (login: "${usernameInputField.value}") {
+                name
+                url
+                avatarUrl
+                bio
+                login
+                    repositories (last: 20) {
+                            nodes {
+                                name
+                                updatedAt
+                                description
+                                forkCount
+                                url
+                                stargazerCount
+                                primaryLanguage {
+                                    name
+                                    color
+                                }
+                            }
+                    }
+            }
+          }`
+      })
+    })
+      const result = await res.json()
+      return {
+        statusCode: 200,
+        body: result
+      }
 }
